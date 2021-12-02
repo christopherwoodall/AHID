3608d4a5b451b4d6584dcfe70e4a2d4a41ca2732
Christopher Woodall <woodall.christopher@gmail.com>
2021-12-02 08:36:55 -0500

    Initial Commit
    Basic usb gadget and webserver

---
+++ network.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+## NOTES
+# https://thepi.io/how-to-use-your-raspberry-pi-as-a-wireless-access-point/
+# https://learn.adafruit.com/setting-up-a-raspberry-pi-as-a-wifi-access-point/install-software

288825c7b24a999cdcfec4f2288eae4d052bb659
Christopher Woodall <woodall.christopher@gmail.com>
2022-04-05 01:15:08 -0400

    Update server.py

--- src/opt/ahid/server.py
+++ src/opt/ahid/server.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import base64
-from pathlib import Path
 import json
 import urllib.parse
+from pathlib import Path
 
 import flask
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, CORS, request, jsonify, render_template
+
+
+__name__ = 'ahid'
 
 
 def render_html(html_file, **kwargs):
     return Path(html_file).read_text(encoding='utf-8')
 
-app = Flask(__name__, static_folder='./html/assets', template_folder='./html')
+
+app = Flask(__name__, static_folder='./frontend/assets', template_folder='./frontend')
+
+# enable CORS
+#CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -35,7 +47,10 @@ if __name__ == '__main__':
     app.run(
         debug = True,
         host="0.0.0.0",
-        ssl_context=('./ssl/cert.pem', './ssl/key.pem')
+        ssl_context=(
+            './backend/ssl/cert.pem',
+            './backend/ssl/key.pem'
+        )
         #ssl_context='adhoc'
     )
 
